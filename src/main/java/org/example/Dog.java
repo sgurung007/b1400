804c513e43fa7f3b9bf64b5e8d7e02eedeb9886c
surajgurung <sgrg007@gmail.com>
2020-06-01 23:17:45 -0500

    animal interface and dog class is added

---
+++ src/main/java/org/example/Dog.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Dog implements Animal {
+    @Override
+    public void nameAnimal() {
+        System.out.println("dog animal");
+    }
+
+    @Override
+    public void typeAnimal() {
+        System.out.println("dog type");
+    }
+}
