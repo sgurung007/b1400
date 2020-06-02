17420915f028f0e6e7e16f8fa1a73373ea3faeb1
surajgurung <sgrg007@gmail.com>
2020-06-02 00:01:52 -0500

    handBranch added

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,21 +2,23 @@ package org.example;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 
-    public void nameDisplay(){
+    public void nameDisplay() {
         System.out.println("display name");
     }
 
-    public void lastNameDisplay(){
+    public void lastNameDisplay() {
         System.out.println("last name display");
+    }
+
+
+    public void handbranch() {
+        System.out.println("hand branch");
     }
 
 }
