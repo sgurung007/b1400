bb79a66eb0e7acd16a470d2162e229528b5ac8fc
surajgurung <sgrg007@gmail.com>
2020-06-02 01:23:10 -0500

    navigateProgrammableControlStepDefinitions.java is added

---
+++ src/test/java/steps/ProgrammableStepDefinitions/navigateProgrammableControlStepDefinitions.java
@@ -0,0 +1,7 @@
+package steps.ProgrammableStepDefinitions;
+
+public class navigateProgrammableControlStepDefinitions {
+    public void display(){
+        System.out.println("display from navigateProgrammableControlStepDefinitions.");
+    }
+}
